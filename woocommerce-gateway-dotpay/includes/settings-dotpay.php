4b433f6ea0596fc77ac702d5fc20f455cc224899
sylwek <sylwek@pc224466.localhost>
2016-03-16 13:25:54 +0100

    add settings masterpass, blik

--- woocommerce-gateway-dotpay/includes/settings-dotpay.php
+++ woocommerce-gateway-dotpay/includes/settings-dotpay.php
@@ -4,6 +4,15 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
+$nameArrayMasterPass = array(
+    __('Show separately in a shop channel', 'dotpay-payment-gateway'),
+    'MasterPass (First Data Polska S.A.)',
+);
+$nameArrayBLIK = array(
+    __('Show separately in a shop channel', 'dotpay-payment-gateway'),
+    'BLIK (Polski Standard Płatności Sp. z o.o.)',
+);
+
 /**
  * Settings for Dotpay Gateway.
  */
@@ -23,6 +32,18 @@ return array(
         'title' => __('Dotpay customer PIN', 'dotpay-payment-gateway'),
         'type' => 'text',
         'default' => '',
+    ),
+    'dotpay_masterpass_show' => array(
+        'title' => __('MasterPass', 'dotpay-payment-gateway'),
+        'type' => 'checkbox',
+        'label' => implode(' ', $nameArrayMasterPass),
+        'default' => 'yes',
+    ),
+    'dotpay_blik_show' => array(
+        'title' => __('BLIK', 'dotpay-payment-gateway'),
+        'type' => 'checkbox',
+        'label' => implode(' ', $nameArrayBLIK),
+        'default' => 'yes',
     ),
     'dotpay_channel_show' => array(
         'title' => __('Widget', 'dotpay-payment-gateway'),
