75c27d84937c841683978021067cbaf91a713332
wpawel <pawel.wojcik@gmail.com>
2016-12-21 09:03:46 +0100

    Update Payment.php

--- woocommerce-dotpay/Dotpay/Payment.php
+++ woocommerce-dotpay/Dotpay/Payment.php
@@ -46,7 +46,7 @@ abstract class Dotpay_Payment extends WC_Payment_Gateway {
     // STR EMPTY
     const STR_EMPTY = '';
     // Module version
-    const MODULE_VERSION = '3.0.6';
+    const MODULE_VERSION = '3.0.7';
     
     public static $ocChannel = 248;
     public static $pvChannel = 248;
